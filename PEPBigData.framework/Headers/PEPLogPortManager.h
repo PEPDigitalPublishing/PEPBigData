69f689d991e2f7052d6217a28c757d702145cf6a
RavenKite <lipeizhuo0528@outlook.com>
2019-09-24 14:01:17 +0800

    update 0.3.0

--- PEPBigData.framework/Headers/PEPLogPortManager.h
+++ PEPBigData.framework/Headers/PEPLogPortManager.h
@@ -11,7 +11,8 @@
 
 @interface PEPLogPortManager : NSObject 
 
-@property (nonatomic,assign) BOOL logFlag;
+@property (nonatomic, assign) BOOL logFlag;
+
 + (instancetype)shareInstance;
 
 /**
@@ -27,8 +28,8 @@
  * 适用于暂无状态及结果的动作。
  * @param actionId 动作id
  * @param object 被动者
- * @param request 请求
- * @param params 参数
+ * @param request 接口请求地址
+ * @param params 接口请求参数
  */
 
 - (void)onEventWithActionId:(NSString *) actionId andObject:(NSString *) object andRequest:(NSString *) request andParams:(NSString *) params;
@@ -50,14 +51,38 @@
  * 适用于全部参数提交。
  * @param actionId 动作id
  * @param object 被动者
- * @param request 请求
- * @param params 参数
+ * @param request 接口请求地址
+ * @param params 接口请求参数
  * @param retCode 状态码
  * @param retInfo 状态详细
  * @param fromPos 来源位置
  */
 
 - (void)onEventWithActionId:(NSString *) actionId andObject:(NSString *) object andRequest:(NSString *) request andParams:(NSString *) params andRetCode:(NSString *) retCode andRetInfo:(NSString *) retInfo andFromPos:(NSString *) fromPos;
+
+
+/**
+ * 适用于异常状态的全部参数提交。跳过内部的数据累积等待逻辑，立即将数据提交给服务器。
+ * @param actionID 动作id
+ * @param requestURL 接口请求地址
+ * @param params 接口请求参数
+ * @param retCode 状态码
+ * @param retInfo 状态详细
+ * @param begin 请求开始时间戳，单位毫秒
+ * @param end 请求响应时间戳，单位毫秒
+ * @param fromPos 来源位置
+ */
+
+- (void)onEventFromExceptionWithActionID:(NSString *)actionID
+                              requestURL:(NSString *)requestURL
+                                  params:(NSString *)params
+                                 retCode:(NSString *)retCode
+                                 retInfo:(NSString *)retInfo
+                          beginTimestamp:(NSTimeInterval)begin
+                            endTimestamp:(NSTimeInterval)end
+                                  object:(NSString *)object
+                                 fromPos:(NSString *)fromPos;
+
 
 
 @end
